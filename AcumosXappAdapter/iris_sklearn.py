44868e9f74b66fb57d04ca30ab9ac522a82e0bb5
Manoop Talasila <talasila@research.att.com>
2019-10-15 14:25:53 -0400

    Add Acumos xApp Adapter
    Patch to address comments about initializing a new repo for first time with all basic required files.
    
    Issue-Id: RICAPP-4
    
    Signed-off-by: Manoop Talasila <talasila@research.att.com>
    Change-Id: Icd18d928f58a601581ac35bafd12b24726e07802

--- AcumosXappAdapter/iris_sklearn.py
+++ AcumosXappAdapter/iris_sklearn.py
@@ -1,20 +1,20 @@
-# ===============LICENSE_START=======================================================
-# Acumos Apache-2.0
-# ===================================================================================
-# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
-# ===================================================================================
-# This Acumos software file is distributed by AT&T and Tech Mahindra
-# under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# ========================LICENSE_START=================================
+#   O-RAN-SC
+#   %%
+#   Copyright (c) 2019 AT&T Intellectual Property.
+#   %%
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#          http://www.apache.org/licenses/LICENSE-2.0
 #
-# This file is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ===============LICENSE_END=========================================================
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ========================LICENSE_END===================================
 
 from acumos.session import AcumosSession
 from acumos.modeling import Model, List, create_dataframe
@@ -53,4 +53,4 @@ model = Model(classify=classify_iris)
 
 session = AcumosSession()
 
-session.dump(model,'iris_sklearn','/Users/guy/Desktop')
+session.dump(model,'iris_sklearn','/temp')
